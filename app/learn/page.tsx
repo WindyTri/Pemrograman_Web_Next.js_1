eb574ff1d21c77d8018f0c3367e52e4d6faaeed7
Windy Tri Nurcahyani <windytri788@gmail.com>
2026-04-29 16:44:48 +0800

    Membuat lapisan div dan memasukkan beberapa element

---
+++ app/learn/page.tsx
@@ -0,0 +1,13 @@
+export default function learnPage () {
+    return(
+        <div>
+            <div>
+             
+            </div>
+            <div>
+                <p>Sigma</p>
+                <img/>
+            </div>
+        </div>
+    );
+}
