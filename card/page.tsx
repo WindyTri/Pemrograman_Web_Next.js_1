42f5bb5447b3522290906eef78c2018102355923
WindyTri <windytri788@gmail.com>
2026-05-18 18:35:26 +0800

    quiz

---
+++ card/page.tsx
@@ -0,0 +1,74 @@
+export default function card () {
+    return(
+        <div className="bg-purple-200 h-screen">
+            <div className="flex justify-center ">
+                {/* kotak 1 */}
+                <div className="bg-white rounded-md h-65 w-55 flex justify-center pt-3">
+                    <div className="bg-blue-200 rounded-md h-50 w-50 p-5 gap-3 ">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex flex-col gap-2">
+                            <h1 className="text-black font-bold text-2xl">Web Design</h1>
+                            <p className="text-black">Craft angaging, user-friendly websites.</p>
+                        </div>
+                        <div className="flex flex-row gap-2">
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5 w-25 flex justify-center">Landing Page</p>
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5 w-20 flex justify-center">Website</p>
+                            <div className="">
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5">One Page</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="pt-16 flex justify-between">
+                            <p className="font-bold text-black ">Explore</p>
+                            <img src="/arrow-narrow-right.svg"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-white rounded-md h-65 w-55 flex justify-center pt-3">
+                    <div className="bg-blue-200 rounded-md h-50 w-50 p-5 gap-3 ">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex flex-col gap-2">
+                            <h1 className="text-black font-bold text-2xl">Web Design</h1>
+                            <p className="text-black">Craft angaging, user-friendly websites.</p>
+                        </div>
+                        <div className="flex flex-row gap-2">
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5 w-25 flex justify-center">Landing Page</p>
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5 w-20 flex justify-center">Website</p>
+                            <div className="">
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5">One Page</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="pt-16 flex justify-between">
+                            <p className="font-bold text-black ">Explore</p>
+                            <img src="/arrow-narrow-right.svg"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-white rounded-md h-65 w-55 flex justify-center pt-3">
+                    <div className="bg-blue-200 rounded-md h-50 w-50 p-5 gap-3 ">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex flex-col gap-2">
+                            <h1 className="text-black font-bold text-2xl">Web Design</h1>
+                            <p className="text-black">Craft angaging, user-friendly websites.</p>
+                        </div>
+                        <div className="flex flex-row gap-2">
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5 w-25 flex justify-center">Landing Page</p>
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5 w-20 flex justify-center">Website</p>
+                            <div className="">
+                            <p className="bg-blue-300 rounded-2xl w-15 text-[11px] text-black h-5">One Page</p>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="pt-16 flex justify-between">
+                            <p className="font-bold text-black ">Explore</p>
+                            <img src="/arrow-narrow-right.svg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
